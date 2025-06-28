ac9ed2a55e57158a9c068907b52ec6d9126dcf8f
鸞瑤綾舞 <105547626+luanyaolingwu@users.noreply.github.com>
2025-06-28 16:28:18 +0800

    Temp v2

--- src/main/java/com/quanweng/shopping/controller/TranslateController.java
+++ src/main/java/com/quanweng/shopping/controller/TranslateController.java
@@ -2,11 +2,10 @@ package com.quanweng.shopping.controller;
 
 import com.quanweng.shopping.pojo.common.Result;
 import com.quanweng.shopping.pojo.Translate;
+import com.quanweng.shopping.pojo.VO.TranslateResponseVO;
 import com.quanweng.shopping.service.TranslateService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,7 +16,6 @@ public class TranslateController {
     @Autowired
     private TranslateService translateService;
 
-
     @GetMapping("/translate")
     public Result getTranslation(@RequestParam String text){
         List<Translate> translateList = translateService.getTranslation(text);
@@ -25,6 +23,14 @@ public class TranslateController {
         return Result.success(translateList);
     }
 
+    @GetMapping("/translate/v2")
+    public Result getTranslationWithMetadata(@RequestParam String text){
+        TranslateResponseVO response = translateService.getTranslationWithMetadata(text);
+        log.info("查询文字{}对应的翻译 - 数据源: {}, 查询时间: {:.6f}ms, 结果数量: {}", 
+                text, response.getDataSource(), response.getQueryUsageTime(), 
+                response.getData() != null ? response.getData().size() : 0);
+        return Result.success(response);
+    }
 
     @PostMapping("/translate")
     public Result createTranslation(@RequestBody Translate translate){
@@ -33,14 +39,12 @@ public class TranslateController {
         return Result.success();
     }
 
-
     @PutMapping("/translate")
     public Result updateTranslation(@RequestBody Translate translate){
         translateService.updateTranslation(translate);
         log.info("更新翻译:{}",translate);
         return Result.success();
     }
-
 
     @DeleteMapping("/translate")
     public Result deleteTranslation(@RequestParam String text){
